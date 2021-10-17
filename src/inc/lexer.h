0cffa08cb1e4359c77f8f205f6f6059317422387
Artur <turods@yandex.ru>
2021-10-17 19:25:32 +0300

    IMPLEMENTED: lexer

---
+++ src/inc/lexer.h
@@ -0,0 +1,66 @@
+#ifndef _LEXER_H
+#define _LEXER_H
+
+#include <string>
+#include <vector>
+
+enum TOKEN_TYPE {
+    TOKEN_IDENT,
+    TOKEN_INT,
+    TOKEN_DOBULE,
+    TOKEN_CHAR,
+    TOKEN_STRING,
+    TOKEN_SET_INT,
+    TOKEN_SET_DOBULE,
+    TOKEN_SET_CHAR,
+    TOKEN_SET_STRING,
+    TOKEN_POP,
+    TOKEN_DUP, 
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_MUL,
+    TOKEN_DIV,
+    TOKEN_EQ,
+    TOKEN_GT,
+    TOKEN_LS,
+    TOKEN_LE,
+    TOKEN_GE,
+    TOKEN_ASSIGN,
+    TOKEN_SET_LABEL,
+    TOKEN_JMP,
+    TOKEN_JME,
+    TOKEN_IF,
+    TOKEN_ELSE,
+    TOKEN_WHILE,
+    TOKEN_BEGIN,
+    TOKEN_END,
+    TOKEN_PRINT,
+    TOKEN_PUTS,
+    TOKEN_INCR,
+    TOKEN_DECR,
+    TOKEN_GET_VAR,
+    TOKEN_EXIT
+};
+
+class Token {
+
+    TOKEN_TYPE type;
+    std::string value;
+
+    public:
+        Token(TOKEN_TYPE);
+        Token(TOKEN_TYPE, std::string);
+        TOKEN_TYPE get_type();
+        std::string get_value();
+};
+
+class Tokenizer {
+
+    std::string source;
+
+    public:
+        Tokenizer(std::string);
+        std::vector<Token> tokenize();
+};
+
+#endif
---
+++ src/lexer.cpp
@@ -0,0 +1,162 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "inc/lexer.h"
+#include "inc/helper.h"
+
+Token::Token(TOKEN_TYPE type) {
+    this->type = type;
+    this->value = "";
+}
+
+Token::Token(TOKEN_TYPE type, std::string value) {
+    this->type = type;
+    this->value = value;
+}
+
+TOKEN_TYPE Token::get_type() {
+    return this->type;
+}
+
+std::string Token::get_value() {
+    return this->value;
+}
+
+Tokenizer::Tokenizer(std::string source) {
+    this->source = source;
+}
+
+std::vector<Token> Tokenizer::tokenize() {
+    std::vector<Token> result;
+
+    std::string word_buffer;    
+
+    bool quotes = false;
+
+    for (char c : this->source) {
+        
+        if (c == '\"' || c == '\'')
+            quotes = !quotes;
+
+        if ((c != ' ' && c != '\n' && c != '\t') || quotes)
+            word_buffer += c;
+        else {
+            if (!word_buffer.empty()) {
+
+                if (helper::is_int(word_buffer))
+                    result.push_back(Token(TOKEN_INT, word_buffer));
+
+                else if (helper::is_double(word_buffer))
+                    result.push_back(Token(TOKEN_DOBULE, word_buffer));
+                
+                else if (word_buffer[0] == '\'')
+                    result.push_back(Token(TOKEN_CHAR, word_buffer.substr(1, word_buffer.length()-2)));
+
+                else if (word_buffer[0] == '\"')
+                    result.push_back(Token(TOKEN_STRING, word_buffer.substr(1, word_buffer.length()-2)));
+
+                else if (word_buffer == "int")
+                    result.push_back(Token(TOKEN_SET_INT));
+                
+                else if (word_buffer == "double")
+                    result.push_back(Token(TOKEN_SET_DOBULE));
+
+                else if (word_buffer == "char")
+                    result.push_back(Token(TOKEN_SET_CHAR));
+
+                else if (word_buffer == "string")
+                    result.push_back(Token(TOKEN_SET_STRING));
+
+                else if (word_buffer == "pop")
+                    result.push_back(Token(TOKEN_POP));
+                
+                else if (word_buffer == "dup")
+                    result.push_back(Token(TOKEN_DUP));
+
+                else if (word_buffer == "+")
+                    result.push_back(Token(TOKEN_PLUS));
+
+                else if (word_buffer == "-")
+                    result.push_back(Token(TOKEN_MINUS));
+
+                else if (word_buffer == "*")
+                    result.push_back(Token(TOKEN_MUL));
+
+                else if (word_buffer == "/")
+                    result.push_back(Token(TOKEN_DIV));
+
+                else if (word_buffer == "==")
+                    result.push_back(Token(TOKEN_EQ));
+
+                else if (word_buffer == ">")
+                    result.push_back(Token(TOKEN_GT));
+
+                else if (word_buffer == "<")
+                    result.push_back(Token(TOKEN_LS));
+
+                else if (word_buffer == "<=")
+                    result.push_back(Token(TOKEN_LE));
+
+                else if (word_buffer == ">=")
+                    result.push_back(Token(TOKEN_GE));
+
+                else if (word_buffer == "=")
+                    result.push_back(Token(TOKEN_ASSIGN));
+
+                else if (word_buffer == "if")
+                    result.push_back(Token(TOKEN_IF));
+
+                else if (word_buffer == "else")
+                    result.push_back(Token(TOKEN_ELSE));
+
+                else if (word_buffer == "while")
+                    result.push_back(Token(TOKEN_WHILE));
+
+                else if (word_buffer == "{")
+                    result.push_back(Token(TOKEN_BEGIN));
+
+                else if (word_buffer == "}")
+                    result.push_back(Token(TOKEN_END));
+
+                else if (word_buffer == "print")
+                    result.push_back(Token(TOKEN_PRINT));
+
+                else if (word_buffer == "puts")
+                    result.push_back(Token(TOKEN_PUTS));
+
+                else if (word_buffer == "++")
+                    result.push_back(Token(TOKEN_INCR));
+
+                else if (word_buffer == "--")
+                    result.push_back(Token(TOKEN_DECR));
+
+                else if (word_buffer == "exit")
+                    result.push_back(Token(TOKEN_EXIT));
+
+                else {
+                    if (word_buffer[word_buffer.length() - 1] == '$') {
+                        result.push_back(Token(TOKEN_IDENT, word_buffer.substr(0, word_buffer.length()-1)));
+                        result.push_back(Token(TOKEN_GET_VAR));
+                    } else if (word_buffer[word_buffer.length() - 1] == ':') {
+                        result.push_back(Token(TOKEN_IDENT, word_buffer.substr(0, word_buffer.length()-1)));
+                        result.push_back(Token(TOKEN_SET_LABEL));
+                    } else 
+                        result.push_back(Token(TOKEN_IDENT, word_buffer));
+                }
+            }
+            word_buffer = "";
+        }
+
+    }
+
+    if (quotes) {
+        std::cerr << "ERROR: Quotes are not closed" << std::endl;
+        exit(-5);
+    }
+
+    for (Token t : result) {
+        std::cout << "TOKEN_TYPE: " << t.get_type() << " VALUE: (" << t.get_value() << ")" << std::endl;
+    }
+    
+    return result;
+}
